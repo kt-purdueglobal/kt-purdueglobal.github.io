b702488200ab5a3e13c11c46f39d47072ebeb5b1
Bailey Wethington <baileywethington@kaplanstudent.onmicrosoft.com>
2024-09-22 14:47:25 -0400

    Added button to designer

--- BookSmart/SearchForm.Designer.cs
+++ BookSmart/SearchForm.Designer.cs
@@ -186,6 +186,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnRecentlyPublished;
         private System.Windows.Forms.DataGridView dataGridViewBooksResult;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button returnHome;
